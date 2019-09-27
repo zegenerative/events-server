4cf3109595dbf35d10397f3f370d86879f49d6ff
zegenerative <zegerdevos@gmail.com>
2019-09-27 13:54:50 +0200

    Create router for Event model

---
+++ event/router.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+const Event = require('../event/model')
+const router = new Router()
+
+//we are going to make routes using the Event model
+
+module.exports = router
