9f4fdde326afb093412157ad469c8f840f8a37f8
zegenerative <zegerdevos@gmail.com>
2019-09-27 13:48:33 +0200

    Create table events

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const db = require('./db')
+// const db = require('./db')
+const Model = require('./event/model')
 
 const port = process.env.PORT || 4000
+
+// app.use(db, Model)
 
 function listenOnPort() {
     console.log(`Listening on port: ${port}`)
